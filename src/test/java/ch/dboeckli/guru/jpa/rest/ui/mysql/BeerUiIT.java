200cd09af3c020b886e57479afc313d121c4f23e
dboeckli <dboeckli@gmail.com>
2025-04-20 14:54:01 +0200

    remove code smell 2

--- src/test/java/ch/dboeckli/guru/jpa/rest/ui/mysql/BeerUiIT.java
+++ src/test/java/ch/dboeckli/guru/jpa/rest/ui/mysql/BeerUiIT.java
@@ -37,14 +37,14 @@ class BeerUiIT {
     private WebDriver webDriver;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");  // Run in headless mode
         webDriver = new ChromeDriver(options);
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         if (webDriver != null) {
             webDriver.quit();
         }
