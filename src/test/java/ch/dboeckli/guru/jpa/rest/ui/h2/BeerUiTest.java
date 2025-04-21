94e68712a198f4c125430515910a49ba9fb671f4
dboeckli <dboeckli@gmail.com>
2025-04-21 14:15:18 +0200

    fix order of ui unit tests for search

--- src/test/java/ch/dboeckli/guru/jpa/rest/ui/h2/BeerUiTest.java
+++ src/test/java/ch/dboeckli/guru/jpa/rest/ui/h2/BeerUiTest.java
@@ -75,190 +75,6 @@ class BeerUiTest {
     }
 
     @Test
-    void testEditBeer() {
-        // Navigate to the beer list page
-        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
-        waitForPageLoad();
-
-        // Find and click the edit button for the first beer
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        WebElement editButton = wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("a[id^='editBeer-']")
-        ));
-        // Extract the beer ID from the edit button's ID
-        String editButtonId = editButton.getAttribute("id");
-        String beerId = StringUtils.substringAfter(editButtonId, "editBeer-");
-
-        editButton.click();
-
-        // Wait for the edit page to load
-        wait.until(ExpectedConditions.urlContains(BEER_PAGE + "/edit/"));
-
-        // Find the beer name input field and update it
-        WebElement beerNameInput = webDriver.findElement(By.id("beerName"));
-        String newBeerName = "Updated Beer Name";
-        beerNameInput.clear();
-        beerNameInput.sendKeys(newBeerName);
-
-        // Submit the form
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
-        try {
-            submitButton.click();
-        } catch (Exception e) {
-            JavascriptExecutor executor = (JavascriptExecutor) webDriver;
-            executor.executeScript("arguments[0].click();", submitButton);
-        }
-
-        // Wait for the beer list page to reload
-        wait.until(ExpectedConditions.urlToBe("http://localhost:" + port + LIST_BEERS_PAGE));
-
-        List<WebElement> beerRows = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#beerTable tbody tr")));
-        log.info("Logging all beer names and IDs:");
-        HashMap<String, String> beerMap = new HashMap<>();
-        for (WebElement row : beerRows) {
-            WebElement nameElement = row.findElement(By.cssSelector("td[id^='beerName-']"));
-            String id = nameElement.getAttribute("id").replace("beerName-", "");
-            String name = nameElement.getText();
-            beerMap.put(id, name);
-            log.info("#### Beer ID: {}, Name: {}", id, name);
-        }
-        log.info("### Checking if the beer name has been updated: {}", beerId);
-        assertEquals(newBeerName, beerMap.get(beerId), "Beer name should be updated in the beer list page");
-    }
-
-    @Test
-    @Order(2)
-    void testCreateNewBeer() {
-        // Navigate to the beer list page
-        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
-        waitForPageLoad();
-
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-
-        // Get the initial total number of beers
-        WebElement totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
-        int initialTotalItems = Integer.parseInt(totalItemsElement.getText());
-        log.info("Initial total items: {}", initialTotalItems);
-
-        // Click on the "Create New Beer" button
-        WebElement newBeerButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("createNewBeer")));
-        newBeerButton.click();
-
-        // Wait for the new beer form to load
-        wait.until(ExpectedConditions.urlContains(BEER_PAGE + "/new"));
-
-        // Fill in the form
-        WebElement beerNameInput = webDriver.findElement(By.id("beerName"));
-        String newBeerName = "Test New Beer " + System.currentTimeMillis();
-        beerNameInput.sendKeys(newBeerName);
-        assertEquals(newBeerName, beerNameInput.getAttribute("value"), "Beer name should be set correctly");
-
-        WebElement beerStyleInput = webDriver.findElement(By.id("beerStyle"));
-        beerStyleInput.sendKeys("IPA");
-
-        WebElement upcInput = webDriver.findElement(By.id("upc"));
-        upcInput.sendKeys("123456789012");
-
-        WebElement priceInput = webDriver.findElement(By.id("price"));
-        priceInput.sendKeys("10.99");
-
-        WebElement quantityOnHandInput = webDriver.findElement(By.id("quantityOnHand"));
-        quantityOnHandInput.sendKeys("100");
-
-        // Validieren Sie die Formulardaten direkt vor dem Absenden
-        String formData = (String) ((JavascriptExecutor) webDriver).executeScript(
-            "return Array.from(document.querySelector('form').elements).map(e => e.name + '=' + e.value).join('&');"
-        );
-        log.info("Form data before submission: {}", formData);
-        // Submit the form
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
-        try {
-            submitButton.click();
-        } catch (Exception e) {
-            JavascriptExecutor executor = (JavascriptExecutor) webDriver;
-            executor.executeScript("arguments[0].click();", submitButton);
-        }
-
-        // Wait for the beer list page to reload
-        wait.until(ExpectedConditions.urlToBe("http://localhost:" + port + LIST_BEERS_PAGE));
-
-        // Check that the total number of beers has increased by 1
-        totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
-        int finalTotalItems = Integer.parseInt(totalItemsElement.getText());
-        log.info("Final total items: {}", finalTotalItems);
-
-        assertEquals(initialTotalItems + 1, finalTotalItems, "Total number of beers should increase by 1 after creation");
-
-        // Navigate to the next page
-        WebElement nextPageButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'page-link') and text()='Next']")));
-        nextPageButton.click();
-        waitForPageLoad();
-
-        // Verify that the new beer is in the list
-        List<WebElement> beerNames = webDriver.findElements(By.cssSelector("td[id^='beerName-']"));
-        log.info("Beer names on the current page:");
-        for (int i = 0; i < beerNames.size(); i++) {
-            WebElement beerNameElement = beerNames.get(i);
-            String beerName = beerNameElement.getText();
-            String beerId = beerNameElement.getAttribute("id").replace("beerName-", "");
-            log.info("{}. Beer ID: {}, Name: {}", i + 1, beerId, beerName);
-        }
-        boolean newBeerExists = beerNames.stream()
-            .anyMatch(element -> element.getText().equals(newBeerName));
-        assertTrue(newBeerExists, "Newly created beer should be present in the list. newBeerName: " + newBeerName);
-    }
-
-    @Test
-    @Order(99)
-    void testDeleteBeer() {
-        // Navigate to the beer list page
-        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
-        waitForPageLoad();
-
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-
-        // Get the initial total number of beers
-        WebElement totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
-        int initialTotalItems = Integer.parseInt(totalItemsElement.getText());
-        log.info("Initial total items: {}", initialTotalItems);
-
-        // Find the delete button for the first beer
-        WebElement deleteButton = wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("button[id^='deleteBeer-']")
-        ));
-        String deleteButtonId = deleteButton.getAttribute("id");
-        String beerId = StringUtils.substringAfter(deleteButtonId, "deleteBeer-");
-
-        // Log the beer being deleted
-        WebElement beerNameElement = webDriver.findElement(By.id("beerName-" + beerId));
-        String beerName = beerNameElement.getText();
-        log.info("Deleting beer: ID = {}, Name = {}", beerId, beerName);
-
-        // Click the delete button
-        ((JavascriptExecutor) webDriver).executeScript("arguments[0].click();", deleteButton);
-
-        // Handle the confirmation dialog
-        wait.until(ExpectedConditions.alertIsPresent());
-        webDriver.switchTo().alert().accept();
-
-        // Wait for the page to reload
-        waitForPageLoad();
-
-        // Check that the total number of beers has decreased by 1
-        totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
-        int finalTotalItems = Integer.parseInt(totalItemsElement.getText());
-        log.info("Final total items: {}", finalTotalItems);
-
-        assertEquals(initialTotalItems - 1, finalTotalItems, "Total number of beers should decrease by 1 after deletion");
-
-        // Verify that the deleted beer is no longer in the list
-        List<WebElement> remainingBeerNames = webDriver.findElements(By.cssSelector("td[id^='beerName-']"));
-        boolean beerStillExists = remainingBeerNames.stream()
-            .anyMatch(element -> element.getText().equals(beerName));
-        assertFalse(beerStillExists, "Deleted beer should not be present in the list");
-    }
-
-    @Test
     @Order(3)
     void testSearchByBeerName() {
         webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
@@ -389,6 +205,191 @@ class BeerUiTest {
 
         // Check if "Back to Beer List" link is present
         assertTrue(webDriver.findElement(By.id("backToListButton")).isDisplayed(), "Back to Beer List button should be visible");
+    }
+
+    @Test
+    @Order(97)
+    void testEditBeer() {
+        // Navigate to the beer list page
+        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
+        waitForPageLoad();
+
+        // Find and click the edit button for the first beer
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement editButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.cssSelector("a[id^='editBeer-']")
+        ));
+        // Extract the beer ID from the edit button's ID
+        String editButtonId = editButton.getAttribute("id");
+        String beerId = StringUtils.substringAfter(editButtonId, "editBeer-");
+
+        editButton.click();
+
+        // Wait for the edit page to load
+        wait.until(ExpectedConditions.urlContains(BEER_PAGE + "/edit/"));
+
+        // Find the beer name input field and update it
+        WebElement beerNameInput = webDriver.findElement(By.id("beerName"));
+        String newBeerName = "Updated Beer Name";
+        beerNameInput.clear();
+        beerNameInput.sendKeys(newBeerName);
+
+        // Submit the form
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
+        try {
+            submitButton.click();
+        } catch (Exception e) {
+            JavascriptExecutor executor = (JavascriptExecutor) webDriver;
+            executor.executeScript("arguments[0].click();", submitButton);
+        }
+
+        // Wait for the beer list page to reload
+        wait.until(ExpectedConditions.urlToBe("http://localhost:" + port + LIST_BEERS_PAGE));
+
+        List<WebElement> beerRows = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#beerTable tbody tr")));
+        log.info("Logging all beer names and IDs:");
+        HashMap<String, String> beerMap = new HashMap<>();
+        for (WebElement row : beerRows) {
+            WebElement nameElement = row.findElement(By.cssSelector("td[id^='beerName-']"));
+            String id = nameElement.getAttribute("id").replace("beerName-", "");
+            String name = nameElement.getText();
+            beerMap.put(id, name);
+            log.info("#### Beer ID: {}, Name: {}", id, name);
+        }
+        log.info("### Checking if the beer name has been updated: {}", beerId);
+        assertEquals(newBeerName, beerMap.get(beerId), "Beer name should be updated in the beer list page");
+    }
+
+    @Test
+    @Order(98)
+    void testCreateNewBeer() {
+        // Navigate to the beer list page
+        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
+        waitForPageLoad();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        // Get the initial total number of beers
+        WebElement totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
+        int initialTotalItems = Integer.parseInt(totalItemsElement.getText());
+        log.info("Initial total items: {}", initialTotalItems);
+
+        // Click on the "Create New Beer" button
+        WebElement newBeerButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("createNewBeer")));
+        newBeerButton.click();
+
+        // Wait for the new beer form to load
+        wait.until(ExpectedConditions.urlContains(BEER_PAGE + "/new"));
+
+        // Fill in the form
+        WebElement beerNameInput = webDriver.findElement(By.id("beerName"));
+        String newBeerName = "Test New Beer " + System.currentTimeMillis();
+        beerNameInput.sendKeys(newBeerName);
+        assertEquals(newBeerName, beerNameInput.getAttribute("value"), "Beer name should be set correctly");
+
+        WebElement beerStyleInput = webDriver.findElement(By.id("beerStyle"));
+        beerStyleInput.sendKeys("IPA");
+
+        WebElement upcInput = webDriver.findElement(By.id("upc"));
+        upcInput.sendKeys("123456789012");
+
+        WebElement priceInput = webDriver.findElement(By.id("price"));
+        priceInput.sendKeys("10.99");
+
+        WebElement quantityOnHandInput = webDriver.findElement(By.id("quantityOnHand"));
+        quantityOnHandInput.sendKeys("100");
+
+        // Validieren Sie die Formulardaten direkt vor dem Absenden
+        String formData = (String) ((JavascriptExecutor) webDriver).executeScript(
+            "return Array.from(document.querySelector('form').elements).map(e => e.name + '=' + e.value).join('&');"
+        );
+        log.info("Form data before submission: {}", formData);
+        // Submit the form
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
+        try {
+            submitButton.click();
+        } catch (Exception e) {
+            JavascriptExecutor executor = (JavascriptExecutor) webDriver;
+            executor.executeScript("arguments[0].click();", submitButton);
+        }
+
+        // Wait for the beer list page to reload
+        wait.until(ExpectedConditions.urlToBe("http://localhost:" + port + LIST_BEERS_PAGE));
+
+        // Check that the total number of beers has increased by 1
+        totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
+        int finalTotalItems = Integer.parseInt(totalItemsElement.getText());
+        log.info("Final total items: {}", finalTotalItems);
+
+        assertEquals(initialTotalItems + 1, finalTotalItems, "Total number of beers should increase by 1 after creation");
+
+        // Navigate to the next page
+        WebElement nextPageButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'page-link') and text()='Next']")));
+        nextPageButton.click();
+        waitForPageLoad();
+
+        // Verify that the new beer is in the list
+        List<WebElement> beerNames = webDriver.findElements(By.cssSelector("td[id^='beerName-']"));
+        log.info("Beer names on the current page:");
+        for (int i = 0; i < beerNames.size(); i++) {
+            WebElement beerNameElement = beerNames.get(i);
+            String beerName = beerNameElement.getText();
+            String beerId = beerNameElement.getAttribute("id").replace("beerName-", "");
+            log.info("{}. Beer ID: {}, Name: {}", i + 1, beerId, beerName);
+        }
+        boolean newBeerExists = beerNames.stream()
+            .anyMatch(element -> element.getText().equals(newBeerName));
+        assertTrue(newBeerExists, "Newly created beer should be present in the list. newBeerName: " + newBeerName);
+    }
+
+    @Test
+    @Order(99)
+    void testDeleteBeer() {
+        // Navigate to the beer list page
+        webDriver.get("http://localhost:" + port + LIST_BEERS_PAGE);
+        waitForPageLoad();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        // Get the initial total number of beers
+        WebElement totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
+        int initialTotalItems = Integer.parseInt(totalItemsElement.getText());
+        log.info("Initial total items: {}", initialTotalItems);
+
+        // Find the delete button for the first beer
+        WebElement deleteButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.cssSelector("button[id^='deleteBeer-']")
+        ));
+        String deleteButtonId = deleteButton.getAttribute("id");
+        String beerId = StringUtils.substringAfter(deleteButtonId, "deleteBeer-");
+
+        // Log the beer being deleted
+        WebElement beerNameElement = webDriver.findElement(By.id("beerName-" + beerId));
+        String beerName = beerNameElement.getText();
+        log.info("Deleting beer: ID = {}, Name = {}", beerId, beerName);
+
+        // Click the delete button
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].click();", deleteButton);
+
+        // Handle the confirmation dialog
+        wait.until(ExpectedConditions.alertIsPresent());
+        webDriver.switchTo().alert().accept();
+
+        // Wait for the page to reload
+        waitForPageLoad();
+
+        // Check that the total number of beers has decreased by 1
+        totalItemsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("totalItems")));
+        int finalTotalItems = Integer.parseInt(totalItemsElement.getText());
+        log.info("Final total items: {}", finalTotalItems);
+
+        assertEquals(initialTotalItems - 1, finalTotalItems, "Total number of beers should decrease by 1 after deletion");
+
+        // Verify that the deleted beer is no longer in the list
+        List<WebElement> remainingBeerNames = webDriver.findElements(By.cssSelector("td[id^='beerName-']"));
+        boolean beerStillExists = remainingBeerNames.stream()
+            .anyMatch(element -> element.getText().equals(beerName));
+        assertFalse(beerStillExists, "Deleted beer should not be present in the list");
     }
 
     private void waitForPageLoad() {
